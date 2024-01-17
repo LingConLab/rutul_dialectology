e169a859c6f77ff16d51fffc5c61c04f57c2b5f7
George Moroz <agricolamz@gmail.com>
2024-01-17 11:16:53 +0300

    update vanya's features

--- scripts/cleaning_data_for_database.R
+++ scripts/cleaning_data_for_database.R
@@ -193,6 +193,27 @@ df %>%
   arrange(feature_id) |> 
   write_csv("data/database.csv", na = "", append = TRUE)
 
+
+# add Vanya's -------------------------------------------------------------
+
+read_csv("data/database.csv", col_select = "feature_id") %>% 
+  distinct() %>% 
+  filter(feature_id == max(feature_id)) %>% 
+  pull(feature_id) ->
+  max_id_in_db
+
+read_csv("data/netkachev_oct23.csv") %>% 
+  filter(!is.na(value)) ->
+  df
+
+df %>% 
+  mutate(feature_id = as.double(factor(feature_title))+max_id_in_db) |> 
+  select(feature_id, feature_title, feature_lexeme, feature_description, 
+         collected, compiled, updated_day, updated_month, updated_year, domain, 
+         settlement, value, stimuli, answer) |> 
+  arrange(feature_id)  |> 
+  write_csv("data/database.csv", na = "", append = TRUE)
+
 # after_merge_fix ---------------------------------------------------------
 
 read_csv("data/database.csv") |> 
