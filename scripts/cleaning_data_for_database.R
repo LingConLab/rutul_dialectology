0c7fd953a10d362f6985847263957e481612a6e3
George Moroz <agricolamz@gmail.com>
2024-01-22 06:45:20 +0300

    add Asya's and 200

--- scripts/cleaning_data_for_database.R
+++ scripts/cleaning_data_for_database.R
@@ -1,7 +1,7 @@
 # asya ergative -----------------------------------------------------------
 
 library(tidyverse)
-df <- read_csv("data/asya_features_15-01-24.csv")
+df <- read_csv("data/asya_features_21-01-24.csv")
 df %>% 
   filter(to_map == 1) %>% 
   mutate(feature_id = as.double(factor(feature_title)),
@@ -203,6 +203,27 @@ read_csv("data/database.csv", col_select = "feature_id") %>%
   max_id_in_db
 
 read_csv("data/netkachev_oct23.csv") %>% 
+  filter(!is.na(value)) ->
+  df
+
+df %>% 
+  mutate(feature_id = as.double(factor(feature_title))+max_id_in_db) |> 
+  select(feature_id, feature_title, feature_lexeme, feature_description, 
+         collected, compiled, updated_day, updated_month, updated_year, domain, 
+         settlement, value, stimuli, answer) |> 
+  arrange(feature_id)  |> 
+  write_csv("data/database.csv", na = "", append = TRUE)
+
+
+# add 200-words-lists -----------------------------------------------------
+
+read_csv("data/database.csv", col_select = "feature_id") %>% 
+  distinct() %>% 
+  filter(feature_id == max(feature_id)) %>% 
+  pull(feature_id) ->
+  max_id_in_db
+
+read_csv("data/rutul_dialects_200.csv") %>% 
   filter(!is.na(value)) ->
   df
 
